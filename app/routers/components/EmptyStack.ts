2b4be904f21a620695de160346920c34d3915010
Furkan Arabacı <furkan.arabaci@smartface.io>
2023-06-20 14:22:09 +0300

    delete old typescript files

--- app/routers/components/EmptyStack.ts
+++
@@ -1,7 +0,0 @@
-import $$EmptyStack from '../.smartface/generated/routers/components/EmptyStack';
-
-export default class EmptyStack extends $$EmptyStack {
-  constructor(basePath: string, name: string, routeIndex: number) {
-    super(basePath);
-  }
-}
--- app/routers/components/NestedStackLibrary.ts
+++
@@ -1,7 +0,0 @@
-import $$NestedStackLibrary from '../.smartface/generated/routers/components/NestedStackLibrary';
-
-export default class NestedStackLibrary extends $$NestedStackLibrary {
-  constructor(basePath: string, name: string, routeIndex: number) {
-    super(basePath);
-  }
-}
