580e3920c50cfcae537e8c0d956e2385d52484a7
Furkan Arabacı <furkan.arabaci@smartface.io>
2023-06-21 16:23:41 +0300

    add another index.rtr, fix pages and formatting

--- app/pages/page1/index.tsx
+++ app/pages/page1/index.tsx
@@ -2,5 +2,12 @@ import React from 'react';
 import Page1Design from '@smartface-generated/pages/page1';
 
 export default (props: any) => {
-  return <Page1Design />;
+  return (
+    <Page1Design
+      pressable1={{
+        onPress: () => props.navigation.navigate('page2'),
+        style: { backgroundColor: 'red' },
+      }}
+    />
+  );
 };
