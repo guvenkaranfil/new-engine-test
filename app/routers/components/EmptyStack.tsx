321e700885227be771076a326728b6f305d343c2
Furkan Arabacı <furkan.arabaci@smartface.io>
2023-06-21 19:06:46 +0300

    add empty but not empty stack

---
+++ app/routers/components/EmptyStack.tsx
@@ -0,0 +1,17 @@
+//------------------------------------------------------------------------------
+//
+//     This code was auto generated.
+//
+//     Manual changes to this file may cause unexpected behavior in your application.
+//     Manual changes to this file will be overwritten if the code is regenerated.
+//
+//------------------------------------------------------------------------------
+
+import React from 'react';
+import $$EmptyStack from '@smartface-generated/routers/components/EmptyStack';
+
+const EmptyStack = () => {
+  return <$$EmptyStack />;
+};
+
+export default EmptyStack;
